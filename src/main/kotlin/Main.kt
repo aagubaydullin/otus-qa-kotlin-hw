464650e80fa7aa7d9a8f617ac5ad1f181fe5eed0
gubaidullinaa <aagubaydullin@gmail.com>
2022-11-18 11:39:08 +0300

    hw01

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+
+
+fun main() {
+    TestRun().runTest(steps = Steps(), test = {test()})
+
+
+}
+
+interface TestRunner {
+    fun <T> runTest(steps: T, test: () -> Unit)
+}
+
+class TestRun: TestRunner {
+    override fun <T> runTest(steps: T, test: () -> Unit) {
+        val stepsBefore = Steps()
+        val stepsAfter = Steps()
+        stepsBefore.beforeTest()
+        test()
+        stepsAfter.afterTest()
+    }
+}
+
+class Steps() {
+    fun beforeTest (){
+        println("Before test.")
+    }
+    fun afterTest () {
+        println("After test.")
+    }
+}
+
+fun test () {
+    println("Test...")
+}
